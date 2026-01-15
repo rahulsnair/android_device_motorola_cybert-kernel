eca4b03d4b032fc9bbd073ed24ef738d4a8ba246
rahulsnair <rahulsnair.vdp@gmail.com>
2026-01-15 19:47:07 +0530

    add generated udapi header tar ball

--- headers/include/generated/compile.h
+++
@@ -1,4 +0,0 @@
-#define UTS_MACHINE		"aarch64"
-#define LINUX_COMPILE_BY	"rahulsnair"
-#define LINUX_COMPILE_HOST	"asustufa15"
-#define LINUX_COMPILER		"aarch64-linux-gnu-gcc (Ubuntu 15.2.0-4ubuntu4) 15.2.0, GNU ld (GNU Binutils for Ubuntu) 2.45"
